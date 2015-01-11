48ba3817de3983fdf9c57dc5a0bf0546612f1b65
John Mertens <john.mertens@gmail.com>
2015-01-11 10:04:18 -0800

    Add a mixin to the react components to make them update when their associated models update. Also refactored the MuralList component to use the Artwork model properly

--- viewer_couchapp/attachments/javascripts/react_components.js
+++ viewer_couchapp/attachments/javascripts/react_components.js
@@ -1,4 +1,37 @@
+// Mixin from https://gist.github.com/ssorallen/7883081
+var ModelUpdateMixin = {
+
+  componentDidMount: function() {
+    this._boundForceUpdate = this.forceUpdate.bind(this, null);
+    this.getBackboneObject().on("all", this._boundForceUpdate, this);
+  },
+
+  componentWillUnmount: function() {
+    this.getBackboneObject().off("all", this._boundForceUpdate);
+  },
+
+  getBackboneObject: function() {
+    return this.props.collection || this.props.model;
+  }
+
+  /*
+  componentWillMount: function(){
+    this.props.model.on("change", (function() {
+      console.log("in component will mount");
+      this.forceUpdate();
+    }.bind(this)));
+  },
+
+  componentWillUnmount: function(){
+    console.log("in component will unmount");
+    this.props.model.off("change");
+  }
+  */
+
+};
+
 var MuralItem = React.createClass({
+
   render: function() {
     return (
       <div className='muralItem'>
@@ -13,12 +46,13 @@ var MuralItem = React.createClass({
 });
 
 var MuralList = React.createClass({
+  mixins: [ ModelUpdateMixin ],
   render: function() {
     console.log(this.props);
-    var muralNodes = this.props.murals.map(function(mural) {
+    var muralNodes = this.props.collection.map(function(mural) {
       return (
-        <MuralItem url={mural['image_urls'][0]} created_at={mural['created_at']}>
-          {mural['artist']}
+        <MuralItem key={mural.cid} url={mural.mainImage()} created_at={mural.get('created_at')}>
+          {mural.get('artist')}
         </MuralItem>
       );
     });
